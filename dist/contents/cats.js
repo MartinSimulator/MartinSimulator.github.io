b2418b04fe62ff8bb46b0e3418a6f37e85ad6e88
MartinSimulator <martin.simulator@gmail.com>
2025-12-26 23:44:24 -0800

    updated cat page body file and css file with new cat-card class for separated sections

--- dist/contents/cats.js
+++ dist/contents/cats.js
@@ -1,10 +1,27 @@
 export const catsBody = `
-    <img src="/public/cat-images/bothCats.png" alt="both cats img" class="bothcatsphoto"/>
-    <p>
-        These two fluffballs are two year old balinese cats. They're from the same litter and we got balinese cats because of their hypoallergenic
-        properties (my dad and brother are allergic to most cats).
-    </p>
+    <div class="cat-card">
+        <img src="/public/cat-images/bothCats.png" alt="both cats img" class="cat-photo"/>
+        <p>
+            These fluffballs are my two year old Balinese cats. They're from the same litter of Balinese cats which we chose because of their hypoallergenic
+            properties (my dad and brother are allergic to most cats). They're spoiled and act like they run the place.
+        </p>
+    </div>
 
+    <div class="cat-card">
+        <img src="/public/cat-images/coco1.png" alt="coco img" class="cat-photo"/>
+        <h2>Coco</h2>
+        <p>
+            The smaller of the two, Coco is very affectionate and constantly begs for treats. She loves to bask in sunlight during the day and she chases small toys around the house by night. Louis often chases her but when he sits on her bed, she doesn't hesitate to bully him out.
+        </p>
+    </div>
+
+    <div class="cat-card">
+        <img src="/public/cat-images/louis1.png" alt="louis img" class="cat-photo"/>
+        <h2>Louis</h2>
+        <p>
+            Named after Louis Armstrong for his raspy voice, Louis is a lazy brick. Despite his beautiful looks, he often spends his days staring at walls and looking confused. It often seems like no thoughts occur behind his large eyes. He loves scratches around the neck and back, but clipping his claws is a nightmare!
+        </p>
+    </div>
 
 `;
 //# sourceMappingURL=cats.js.map
